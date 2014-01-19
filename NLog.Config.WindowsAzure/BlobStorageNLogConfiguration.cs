335c88c9f9905c2b30ca42f8157851c417b4a658
Graeme Foster <graemef@gmail.com>
2014-01-19 16:28:43 +0000

    Added initial implemetation.

---
+++ NLog.Config.WindowsAzure/BlobStorageNLogConfiguration.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Xml;
+
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace NLog.Config.WindowsAzure
+{
+    public class BlobStorageNLogConfiguration
+    {
+        private const string ConnectionStringSettingName = "NLogConfig.ConnectionString";
+        private const string ContainerNameSettingName = "NLogConfig.ContainerName";
+        private const string BlobNameSettingName = "NLogConfig.BlobName";
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private readonly string blobName;
+        private readonly string connectionString;
+        private readonly string containerName;
+
+        public BlobStorageNLogConfiguration(string connectionString,
+                                            string containerName,
+                                            string blobName)
+        {
+            this.connectionString = connectionString;
+            this.containerName = containerName;
+            this.blobName = blobName;
+        }
+
+        public static void ConfigureLoggingWithBlobSpecifiedBySettings()
+        {
+            if (IsNLogConfigSpecified())
+                new BlobStorageNLogConfiguration(CloudConfigurationManager.GetSetting(ConnectionStringSettingName),
+                                                 CloudConfigurationManager.GetSetting(ContainerNameSettingName),
+                                                 CloudConfigurationManager.GetSetting(BlobNameSettingName))
+                    .ConfigureLogging();
+            else
+                Logger.Info(
+                    "Blob for NLog configuration is not specified so configuration is not being loaded.\nSet {0}, {1} and {2} in your cloud service configuration to specify an NLog configuration file in blob storage.",
+                    ConnectionStringSettingName,
+                    ContainerNameSettingName,
+                    BlobNameSettingName);
+        }
+
+        private static bool IsNLogConfigSpecified()
+        {
+            return !(string.IsNullOrWhiteSpace(ConnectionStringSettingName) ||
+                     string.IsNullOrWhiteSpace(ContainerNameSettingName) ||
+                     string.IsNullOrWhiteSpace(BlobNameSettingName));
+        }
+
+        public void ConfigureLogging()
+        {
+            ConfigureLoggingWithBlob(GetBlobReference());
+        }
+
+        private CloudBlockBlob GetBlobReference()
+        {
+            Logger.Debug("Getting reference to blob \"{0}\" in container \"{1}\" using connection string \"{2}\".",
+                         this.blobName,
+                         this.containerName,
+                         this.connectionString);
+
+            return CloudStorageAccount.Parse(this.connectionString)
+                                      .CreateCloudBlobClient()
+                                      .GetContainerReference(this.containerName)
+                                      .GetBlockBlobReference(this.blobName);
+        }
+
+        private static void ConfigureLoggingWithBlob(CloudBlockBlob blob)
+        {
+            XmlLoggingConfiguration configuration = ReadLoggingConfiguration(blob);
+
+            Logger.Debug("Applying new configuration to NLog.");
+            LogManager.Configuration = configuration;
+            Logger.Info("NLog is now using the configuration loaded from {0}.", blob.Uri);
+        }
+
+        private static XmlLoggingConfiguration ReadLoggingConfiguration(CloudBlockBlob blob)
+        {
+            Logger.Debug("Opening blob for reading.");
+            Stream stream = blob.OpenRead();
+
+            Logger.Debug("Reading logging configuration from blob.");
+            return new XmlLoggingConfiguration(new XmlTextReader(stream), null);
+        }
+    }
+}
---
+++ NLog.Config.WindowsAzure/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NLog.Config.WindowsAzure")]
+[assembly: AssemblyDescription("Configure NLog logging using configuration files in Blob Storage.")]
+[assembly: AssemblyProduct("NLog.Config.WindowsAzure")]
+[assembly: AssemblyCopyright("Copyright © Graeme Foster 2014")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59a76297-0afc-4a9a-9afc-5e1228016b7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
